3495e39095a9e7be994a7e5dfae0098be87330df
octubre1100 <136340911+octubre1100@users.noreply.github.com>
2023-08-25 09:12:18 -0300

    practicado

--- main.js
+++ main.js
@@ -1,30 +1,59 @@
-//punto 2
-console.log(1000 + 7)
-console.log(1000 - 7)
-console.log(1000 * 7)
-console.log(1000 / 7)
-//punto 3
-let suma = 1000 + 7
-let resta = 1000 - 7
-let multiplicacion = 1000 * 7
-let division = 1000 / 7
-console.log("Variable suma", suma,"Variable resta", resta,"Variable multiplicación", multiplicacion,"Variable división", division)
-alert(" Variable suma " + suma + " Variable resta " + resta + " Variable multiplicación " + multiplicacion +" Variable división " + division)
-//punto 4
-alert("Su edad es de " + (2023-2008))
-//punto 5
-let yearBorn = prompt("Ingrese su año de nacimiento") 
-alert("Su edad es " + (2023-yearBorn))
-//punto 6
-let nombre = prompt("ingrese su nombre")
-alert("Bienvenido usuario " + nombre) 
-//punto 7
-let nombreAlumno = prompt("ingrese su nombre")
-let apellidoAlumno = prompt("ingrese su apellido")
-alert("Bienvenido " + nombreAlumno +" " + apellidoAlumno)
-console.log(nombreAlumno, apellidoAlumno)
-//punto 8
-let diez = 10
-let tres = 3
-let resultadoMultiplicacion = 10 * 3
-alert("variable resultado de multiplicación= " + resultadoMultiplicacion)
+function promediar(number1, number2, number3) {
+let promedio = (number1 + number2 + number3) / 3 
+return promedio
+}
+function elevarcubo(numero) {
+    let cubo = numero * numero * numero
+    return cubo
+}
+let saldo = 0
+function depo1(id) {
+   console.log("tu saldo", saldo = saldo + 1) ;
+    alert("Se ha depositado con exito, saldo actual " + saldo);
+}
+function depo10(id) {
+    console.log("tu saldo", saldo = saldo + 10) ;
+     alert("Se ha depositado con exito, saldo actual " + saldo);
+ }
+ function depo50(id) {
+    console.log("tu saldo", saldo = saldo + 50) ;
+     alert("Se ha depositado con exito, saldo actual " + saldo);
+ }
+ function depo100(id) {
+    console.log("tu saldo", saldo = saldo + 100) ;
+     alert("Se ha depositado con exito, saldo actual " + saldo);
+ }
+ function depo1000(id) {
+    console.log("tu saldo", saldo = saldo + 1000) ;
+     alert("Se ha depositado con exito, saldo actual " + saldo);
+ }
+ function saludo() {
+     let nombre = prompt("ingrese su nombre")
+     let apellido = prompt("ingrese su apellido")
+     alert("bienvenid@ " + nombre + " " + apellido)
+ }
+ function multiplicar() {
+     let n1 = prompt("ingrese numero a multiplicar")
+     let n2 = prompt("ingrese numero por cual multiplicar")
+     alert(n1 * n2)
+ }
+ function division() {
+    let n1 = prompt("ingrese numero a dividir")
+    let n2 = prompt("ingrese numero por cual dividir")
+    alert(n1 / n2)
+}
+function sumar() {
+    let a = prompt("ingrese numero a sumar")
+    let b = prompt("ingrese numero para sumar")
+    let resultado = (parseFloat(a) + parseFloat (b))
+    alert(resultado)
+}
+function restar() {
+    let n1 = prompt("ingrese numero a restar")
+    let n2 = prompt("ingrese numero para restar")
+    alert(n1 - n2)
+}
+function idk() {
+    let n1 = prompt("ingrese numero")
+    alert(n1 * n1 + 5)
+}
