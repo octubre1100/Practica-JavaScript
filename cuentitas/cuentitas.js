36dd80bacd3d4a19d312dd52b1e68b7bebe266bf
octubre1100 <136340911+octubre1100@users.noreply.github.com>
2023-08-31 17:38:17 -0300

    tarea

---
+++ cuentitas/cuentitas.js
@@ -0,0 +1,36 @@
+function elevarcubo(numero) {
+    let cubo = numero * numero * numero
+    return cubo
+}
+function elevar2(numero) {
+    let cuadrado = numero * numero
+    return cuadrado
+}
+function cuenta1(x) {
+    let resultado = elevar2(x) - x - 10
+    return resultado
+}
+function cuenta2(x) {
+    let resultado = (x - 3) * 2
+    return resultado
+}
+function cuenta3(x) {
+    let resultado = x * elevar2(x) + 20
+    return resultado
+}
+function cuenta3_2(x) {
+    let resultado = elevarcubo(x) + 20
+    return resultado
+}
+function cuenta4(x) {
+    let resultado = (x / 2) + 5
+    return resultado
+}
+function cuenta5(x) {
+    let resultado = elevarcubo(x) + elevar2(x) + 10
+    return resultado
+}
+function cuenta6(x) {
+    let resultado = (x * 10) * 5
+    return resultado
+}
